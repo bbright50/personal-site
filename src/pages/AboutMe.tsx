dbbf8b195094f10ee07509268a7126134e24c095
bbright50 <165122465+bbright50@users.noreply.github.com>
2024-08-01 06:51:37 -0600

    Updated AboutMe

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -4,17 +4,47 @@ export default function AboutMe() {
     return (
         <div className="flex flex-col items-center bg-[url('src/assets/white-w-yellow-bg.svg')] w-full text-2xl pt-10 md:pt-20">
             <h1 className="font-bold text-4xl">About Me</h1>
+            <h1 className="font-bold text-4xl">Personal</h1>
             <p className="italic text-lg m-10">
-                I am love being a husband and father more than anything! My family is at
-                the core of who I am, and they are more than I deserve, but that
-                isn&apos;t quite why you are here. I have had a great career in sales,
-                but I have always wanted to do something much more technical. I figured
-                that this point in time in my life is better than any to make the jump.
-                Since March of 2024 I have been finishing a more formal training in
-                coding and looking to be able to contribute to Successr.ai, and aide in
-                the development of their company. I have always been a team player that
-                has played a key role in why the teams I am on finish at the top of the
-                company, and I am eager to learn how I can do so in a dev role!
+                I am a husband and a father of 4. This site won&apos;t mention much about them
+                because I like to keep my family life more private. I am very much the
+                stereotypical dad, with the lawncare, grilling, and new balance shoes.
+                I think it is fun to take care of our home, to build things, and do projects
+                around the house. I love being active, and swimming has become a great
+                interest of mine lately, and I have so much more respect for even mediocre swimmers.
+            </p>
+            <h1 className="font-bold text-4xl">Professional</h1>
+            <p className="italic text-lg m-10">
+                The only thing that has been constant in my journey has been change.
+                From leaving college with only one major class and duplicate general
+                credits that wouldn't transfer, because of family needs, to now making
+                the jump from software sales to software development so that I can
+                work in a field that I am truly passionate about, it has been a wild ride.
+                My wife is a saint for sticking with me through it.
+                <br />
+                <br />
+                I had an excellent career in sales that taught me so many different things.
+                I have always had a natural ability to talk with others, and sales seemed
+                like a great choice after suddenly needing to drop out to provide for our
+                new family. My years in sales taught me about people, but more about myself
+                and how to work hard, be effective, and contribute to top performing teams.
+                I always perfomed above average, but in my last few roles I was really able
+                to be the top performer at each company I was at. I did well, but because I
+                wasn't passionate about sales, I felt I needed to make a change.
+                <br />
+                <br />
+                I have always been interested in tech, and I love building things, but
+                I never took the chance to pursue software. Growing up my family was always
+                on the forefont of technology. We had a LAN in our house before most companies
+                did. When I started toying with the idea of a career change, I started learning,
+                on my own, the basics of HTML, CSS, and Javascript. Once I knew I would be
+                making a change, I started looking for coding bootcamps that could help me
+                not just learn the code, but how to use it in a career role. I was fortunate
+                to land an internship and let me continue to learn and not only expand my knowledge,
+                but get a good amount of practice in a startup setting. Now that I have received a more
+                formal training in a handful of coding languages and frameworks, I am
+                ready to move on from my current position as an intern to a Junior Developer role.
+                Here's to a(nother) brand-new journey!
             </p>
         </div>
     );
